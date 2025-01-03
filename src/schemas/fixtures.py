5173ff7d22a9c6df79eddf2b41386731a1926cbf
Matthew Flegg <fleggsy03@gmail.com>
2025-01-03 16:17:16 +0000

    simplify fixture schema

--- src/schemas/fixtures.py
+++ src/schemas/fixtures.py
@@ -3,10 +3,15 @@ from pydantic import BaseModel
 
 class Fixture(BaseModel):
     id: int
-    home_team: str
-    away_team: str
-    home_score: int
-    away_score: int
+    minutes: int
+    home_team_id: int
+    away_team_id: int
+    home_team_name: str
+    away_team_name: str
+    home_team_score: int
+    away_team_score: int
+    home_team_red_cards: int
+    away_team_red_cards: int
 
 class GetFixturesResponse(BaseModel):
     fixtures: list[Fixture]
