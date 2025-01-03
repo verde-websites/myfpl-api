eb09988322402b580feb134ab6cc9a4b747bc33e
Matthew Flegg <fleggsy03@gmail.com>
2025-01-03 16:53:35 +0000

    add crud fixtures

---
+++ src/crud/fixtures.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.models import Fixture as FixtureDBModel
+
+async def get_fixtures_by_gameweek_id(db: AsyncSession, gameweek_id: int):
+    fixtures_query = await db.execute(select(FixtureDBModel).where(FixtureDBModel.game_week_id == gameweek_id))
+    fixtures_list = fixtures_query.scalars().all()
+    print(fixtures_list)
+    return fixtures_list
