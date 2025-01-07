63e6bca7dc0b35abd6dff8850a4f2b01c797fdad
Sam Wade <wadee732@gmail.com>
2025-01-07 21:46:09 +0000

    added framework for using apscheduler

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 """Creating the FastAPI app"""
 from fastapi import FastAPI
 from .router import build_router
-
+from .lifecycle import apschedule_lifespan
 def build_app():
     """
     Creates the FastAPI app
     """
-    app = FastAPI()
+    app = FastAPI(lifespan=apschedule_lifespan)
     app.include_router(build_router())
+
     return app
+
